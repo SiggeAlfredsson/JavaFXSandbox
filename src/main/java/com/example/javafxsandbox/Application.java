8e842d31140630b1c0a25403ca2d96af63152ff2
SiggeAlfredsson <saorust@hotmail.com>
2022-12-19 08:13:25 +0100

    Added fxml file and some misc

--- src/main/java/com/example/javafxsandbox/Application.java
+++ src/main/java/com/example/javafxsandbox/Application.java
@@ -1,10 +1,10 @@
 package com.example.javafxsandbox;
 
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
-
-import java.io.IOException;
 
 public class Application extends javafx.application.Application {
 
@@ -15,7 +15,15 @@ public class Application extends javafx.application.Application {
     @Override
     public void start(Stage stage) throws Exception {
 
+        Parent root = FXMLLoader.load(getClass().getResource("/sample.fxml"));
+        Scene startScene = new Scene(root);
 
+
+        stage.setTitle("JavaFX Sandbox!");
+        stage.getIcons().add(new Image("icon.png"));
+
+
+        stage.setScene(startScene);
         stage.show();
     }
 }
