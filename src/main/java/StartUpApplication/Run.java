754f46f2957bba5a59545bb36cdb92a63fc27ea6
Sigge <saorust@hotmail.com>
2022-12-24 13:55:35 +0100

    added run to create .jar exc

---
+++ src/main/java/StartUpApplication/Run.java
@@ -0,0 +1,9 @@
+package StartUpApplication;
+
+import java.lang.reflect.AccessibleObject;
+
+public class Run {
+    public static void main(String[] args) {
+        Application.main(args);
+    }
+}
